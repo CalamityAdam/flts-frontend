10a606a9fdd02b41941e0bf853734341c54140f4
Adam Sisk <adamsisk91@gmail.com>
2019-10-14 22:04:36 -0500

    routes reintroduced after netlify testing

--- src/index.js
+++ src/index.js
@@ -29,9 +29,9 @@ const reducer = (state, action) => {
 ReactDOM.render(
   <StateProvider initialState={initialState} reducer={reducer}>
     <Router>
-      {/* <Redirect path='/:slug'/> */}
-      {/* <NotFound path='notfound' /> */}
-      <App />
+      <Redirect path='/:slug' />
+      <NotFound path='notfound' />
+      <App default path='/' />
     </Router>
   </StateProvider>,
   document.getElementById('root')
